36f326a7aaa04ab48933345cba29c8e0e1aecc2b
Yauhen <penkin.evgeniy@gmail.com>
2017-04-13 21:29:04 +0300

    MLab DBs added

--- serv.js
+++ serv.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const app = express();
 const mongojs = require('mongojs');
-const dbPosts = mongojs('bloglist', ['bloglist']);
-const dbComments = mongojs('commentlist', ['commentlist']);
 const bodypParser = require('body-parser');
 
+//let connectionString = 'mongodb://bloglist:bloglist@ds111549.mlab.com:11549/bloglist';
+ let connectionString = 'mongodb://localhost:27017/bloglist';
+const app = express();
+const db = mongojs(connectionString, ['bloglist', 'commentlist']);
+const postsCollection = db.collection('bloglist');
+const commentCollection = db.collection('commentlist');
 
-// app.use(express.static(__dirname + '/dist'));
 app.use(bodypParser.json());
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
-  // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
   }
@@ -23,39 +24,39 @@ app.all('*', function (req, res, next) {
 });
 
 app.get('/bloglist', function (req, res) {
-  dbPosts.bloglist.find(function (err, docs) {
+  postsCollection.find(function (err, docs) {
     res.json(docs);
   });
 });
 
 app.post('/bloglist', function (req, res) {
-  dbPosts.bloglist.insert(req.body, function (err, docs) {
+  postsCollection.insert(req.body, function (err, docs) {
     res.json(docs);
   });
 });
 
 app.get('/bloglist/:id', function (req, res) {
   let id = req.params.id;
-  dbPosts.bloglist.findOne({_id: mongojs.ObjectId(id)}, function (err, docs) {
+  postsCollection.findOne({_id: mongojs.ObjectId(id)}, function (err, docs) {
     res.json(docs);
   });
 });
 
 app.get('/commentlist', function (req, res) {
-  dbComments.commentlist.find(function (err, docs) {
+  commentCollection.find(function (err, docs) {
     res.json(docs);
   });
 });
 
 app.get('/commentlist/:post_id', function (req, res) {
   let post_id = req.params.post_id;
-  dbComments.commentlist.find({post_id: post_id}, function (err, docs) {
+  commentCollection.find({post_id: post_id}, function (err, docs) {
     res.json(docs);
   });
 });
 
 app.post('/commentlist', function (req, res) {
-  dbComments.commentlist.insert(req.body, function (err, docs) {
+  commentCollection.insert(req.body, function (err, docs) {
     res.json(docs);
   });
 });
